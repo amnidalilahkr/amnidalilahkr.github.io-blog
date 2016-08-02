c2cd2e072336cdb425787a3b1c13d1a379f5389a
amnidalilahkr <amni.dalilah@gmail.com>
2016-08-02 16:00:50 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,7 @@ $(function() {
  
     Parse.$ = jQuery;
  
-    Parse.initialize("Vqsv7cfmWca7IssHw8JnXtWhxoIadZUp6xM9KiAw" , "mX4CSDl4MHmymyR12hXCTbsG3mdPRE7a35AK5DGJ");
+    Parse.initialize("Vqsv7cfmWca7IssHw8JnXtWhxoIadZUp6xM9KiAw" , "kMes5CqaDnmX0dqljRnmbykFh7knrakELq7EfpMB");
     Parse.serverURL = 'http://YOUR_PARSE_SERVER:1337/parse'
  
     var TestObject = Parse.Object.extend("TestObject");
