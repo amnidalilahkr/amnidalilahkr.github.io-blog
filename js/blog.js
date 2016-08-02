cc0e269d7cfc950496c3a8f3bf934cd068d10ad8
amnidalilahkr <amni.dalilah@gmail.com>
2016-08-02 17:19:44 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -5,10 +5,19 @@ $(function() {
     Parse.initialize("Vqsv7cfmWca7IssHw8JnXtWhxoIadZUp6xM9KiAw" , "8UhN1Gbt9RBS5Rs77pJlyVqEvYBYNgG1m4zh2UKR");
    // Parse.serverURL = 'http://YOUR_PARSE_SERVER:1337/parse'
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
+   var Blog = Parse.Object.extend("Blog");
+   
+   var Blogs = Parse.collection.extend({ model: Blog});
+   
+   var blogs =new Blogs();
+   
+   blogs.fetch({
+    success: function(blogs){
+     console.log(blogs);
+    },
+    error: function(blogs, error){
+     console.log(error);
+    }
+   });
  
 });
