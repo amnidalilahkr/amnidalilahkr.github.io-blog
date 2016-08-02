c71e8c386d3f757f2e45d1b17c8f550e0b90386d
amnidalilahkr <amni.dalilah@gmail.com>
2016-08-02 15:20:07 +0800

    Create blog.js

---
+++ js/blog.js
@@ -0,0 +1,14 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    Parse.initialize("Vqsv7cfmWca7IssHw8JnXtWhxoIadZUp6xM9KiAw");
+    Parse.serverURL = 'http://YOUR_PARSE_SERVER:1337/parse'
+ 
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+      alert("yay! it worked");
+    });
+ 
+});
